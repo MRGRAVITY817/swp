5dc2864f6a404dd974321e5ecfc59e194fe0edbe
Hoon Wee <mrgravity817@gmail.com>
2024-04-27 00:19:02 +0900

    feat: add tailwind.pest file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "tailwind.pest"]
+struct TailwindParser;
+
+pub fn parse_tailwind(input: &str) -> Result<Vec<&str>, Box<dyn std::error::Error>> {
+    let mut style_list = TailwindParser::parse(Rule::style_list, input)?;
+    let str_style_list = style_list
+        .next()
+        .unwrap()
+        .into_inner()
+        .map(|style| style.as_str())
+        .collect::<Vec<&str>>();
+
+    Ok(str_style_list)
 }
 
 #[cfg(test)]
@@ -7,8 +22,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_parse_naive_styles() {
+        let input = "bg-red-500 text-white container";
+
+        let output = parse_tailwind(input);
+
+        assert!(output.is_ok());
+        assert_eq!(
+            output.unwrap(),
+            vec!["bg-red-500", "text-white", "container"]
+        );
     }
 }
